fe24d81e094436538835dd0b99d59f00581d7382
kaitlintan <42954090+kaitlintan@users.noreply.github.com>
2018-09-10 14:13:38 -0700

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,23 @@
+// AboutMe.java by Kaitlin Tan
+// 9/10/18
+
+import java.util.*;
+
+public class AboutMe{
+
+    public static String name;
+    public static String hairColor;
+    public static Integer age;
+
+    public static AboutMe() {
+      name = "Kaitlin";
+      hairColor = "Black";
+      age = 16;
+    }
+
+public static vois main(String[] args) {
+
+  AboutMe kt = new AboutMe();
+  System.out.println("Hello my name is " + kt.name);
+  
+}
