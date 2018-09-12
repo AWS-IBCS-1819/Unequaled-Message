ab8aa5bec03c09f3e7a911e0c6ed3df6e9116894
Kaitlin Tan <kaitlin_tan@m018049.aw.org>
2018-09-12 13:23:52 -0700

    hi

--- AboutMe.java
+++ AboutMe.java
@@ -5,19 +5,31 @@ import java.util.*;
 
 public class AboutMe{
 
-    public static String name;
-    public static String hairColor;
-    public static Integer age;
+    public static String lefthandedOrRighthanded;
+    public static String lettersInFullName;
+    public static Integer height;
+    public static Integer favoriteClass;
+    public static Integer bestPlaceTraveledTo;
+    public static Integer hobbies;
+    public static Integer favoriteFood;
 
-    public static AboutMe() {
-      name = "Kaitlin";
-      hairColor = "Black";
-      age = 16;
+
+    public AboutMe() {
+      lefthandedOrRighthanded = "righthanded";
+      lettersInFullName = "9";
+      height = 160;
+      favoriteClass = bio;
+      bestPlaceTraveledTo = Osaka;
+      hobbies = swimming, playing
+
     }
 
 public static vois main(String[] args) {
 
   AboutMe kt = new AboutMe();
   System.out.println("Hello my name is " + kt.name);
-  
+
+  public static vois main(String[] args) {
+
+  }
 }
